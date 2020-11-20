bf1248b021b9eeeda890008d42460536bbc98066
Thraac <nathanofwise@gmail.com>
2020-11-19 20:39:34 -0700

    starting to look at playlist creation

--- script.py
+++ script.py
@@ -1,8 +1,20 @@
 import requests, json
 import keys
 
-clientID = keys.client_id
+clientId = keys.client_id
 clientSecret = keys.client_secret
 
-class buildPlaylist():
-    pass
+class BuildPlaylist():
+    
+    def __init__(self):
+        self.clientId = clientId
+
+    def createPlaylist(self):
+
+        request_body = json.dump({
+                "name": "Discord Playlist",
+                "description": "Playlist related to discord chat",
+                "public": False
+        })
+
+        query = f"https://api.spotify.com/v1/users/{clientId}/playlists"
