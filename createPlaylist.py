9239a96aa58877eb05d952d004064a77e23a768f
Nathan Wise <nathanofwise@gmail.com>
2022-01-29 21:08:04 -0700

    adding secrets and trying searches

--- createPlaylist.py
+++ createPlaylist.py
@@ -1,12 +1,33 @@
-import json
+import json, requests
+from secrets import user_id, spotify_token
 
 class CreatePlaylist:
 
     def __init__(self) -> None:
+        self.user_id = user_id
+        self.spotify_token = spotify_token
+
+    def checkPlaylists(self):
+        check_query = "https://api.spotify.com/v1/users/{self.user_id}/playlists"
+
+        # request_body = json.dumps {
+            
+        # }
+
+        response = requests.get(
+            check_query,
+            # data=request_body,
+            headers={
+                "Authorization" : "Bearer {self.spotify_token}",
+                "Content-Type": "application/json"
+            }
+        )
+
+        response_json = response.json()
+        print(response_json)
+
+    def createPlayList(self):
         pass
 
-    def checkPlaylists():
-        pass
-
-    def createPlayList():
-        pass
+test = CreatePlaylist()
+test.checkPlaylists()
